a27b4b1ad9a08320859236503c50a56203e233ec
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-10-02 20:44:55 -0700

    project completed

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,50 +1,57 @@
+const User = require("../users/users-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
-  const methode = req.methode;
-  const url = req.url;
-  const time = new Date().toISOString();
-  console.log(`'${methode} ${url} ${time}`)
+  const timeStamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`'${timeStamp} ${method} to ${url}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  const id = req.params.id;
-  user.findById(id, (err, user) => {
-    if(err) {
-      next(err);
-    } else if (user) {
-      req.user = user
-      next();
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id);
+    if(!user){
+     next({status: 404, message: 'user not found'})
     } else {
-      res.status(404).json({message: "user not found"});
+      req.user = user
+      next()
     }
-  });
-}
+  } catch (err) {
+    res.status(500).json({
+      message: 'problem finding user',
+    })
+  }
+  }
+
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-  const body = req.body;
-  if(body.name) {
-    next();
+  const { name } = req.body;
+  if (!name || !name.trim) {
+    res.status(400).json({ message: "missing required name field" });
   } else {
-    res.status(400).json({message: "missing required name field"});
+    req.name = name.trim()
+    next()
   }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  const body = req.body;
-  if(body.text){
-    next();
+  const { text } = req.body;
+  if (!text || !text.trim) {
+    res.status(400).json({ message: "missing required text field" });
   } else {
-    res.status(400).json({message: "missing required text field"});
+    req.text = text.trim()
+    next()
   }
 }
 
 // do not forget to expose these functions to other modules
-module.export = {
+module.exports = {
   validateUser,
   validateUserId,
   validatePost,
   logger,
-}
+};
